73417ad4b57255e8be2805701466f83f96c6c3c7
KillianRvt <killian.riveault@ynov.com>
2017-11-21 17:16:23 +0100

    Exercise 5

--- src/exercise5.js
+++ src/exercise5.js
@@ -1,3 +1,18 @@
 exports.run = function(input) {
   console.log("Exercise #5");
 };
+
+
+var fs = require('fs');
+var path = require('path');
+
+function run(directory){
+
+  fs.readdir (directory, function callback (err, list){
+      for (var i=0; i<list.length;i++){
+          console.log(list[i])
+      }
+  });
+}
+
+fs.readdir(process.argv[2],callback);
