ee943fb06d69e910b6350f4d3b62acea0d40ecc7
KillianRvt <killian.riveault@ynov.com>
2017-11-21 16:45:50 +0100

    Exercise 2 avec fonction à part

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,3 +1,22 @@
-exports.run = function(input) {
+exports.run = function run(input) {
+  if(input == 0){ return 0;}
+  else{
+    var remainder = input % 10
+    return Math.max(remainder, biggestNumber((input-remainder)*1e-1));
+  }
   console.log("Exercise #2");
 };
+
+
+
+
+// Fonction qui fonctionne et retour le plus grand nombre mais impossible avec exports
+function run(input){
+  if(input == 0){ return 0;}
+  else{
+    var remainder = input % 10
+    return Math.max(remainder, run((input-remainder)*1e-1));
+  }
+}
+
+console.log(run(1871815))
