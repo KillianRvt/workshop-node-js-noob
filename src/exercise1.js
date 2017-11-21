87d4dbd15795671f0dc7723bdfcabc1c624dc2e6
KillianRvt <killian.riveault@ynov.com>
2017-11-21 16:26:17 +0100

    Exercice 1 avec en plus une fonction sans exports

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,3 +1,13 @@
-exports.run = function(input) {
-  console.log("Exercise #1");
+exports.run = function run (input){
+  console.log(input.toUpperCase());
 };
+
+
+
+
+// Création d'une fonction maj qui répond à l'énoncé mais sans le exports car impossible de tester avec exports
+// function maj (input) {
+//   console.log(input.toUpperCase());
+// }
+
+// maj("Bonjour")
